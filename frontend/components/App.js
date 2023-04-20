1bb2c1e80fe8e01648aa72cf6330a68f8401e625
Grayson Shogan <graysontoddshogan@gmail.com>
2023-04-20 13:56:26 -0700

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -13,17 +13,22 @@ export default class App extends React.Component {
     const { value } = evt.target;
     this.setState({ ...this.state, todoNameInput: value });
   };
+  resetForm = () => {
+    this.setState({ ...this.state, todoNameInput: "" });
+  };
+
+  setAxiosResponseError = (err) => {
+    this.setState({ ...this.state, error: err.response.data.message });
+  };
 
   postNewTodo = () => {
     axios
       .post(URL, { name: this.state.todoNameInput })
       .then((res) => {
         this.fetchAllTodos();
-        this.setState({ ...this.state, todoNameInput: "" });
+        this.resetForm();
       })
-      .catch((err) => {
-        this.setState({ ...this.state, error: err.response.data.message });
-      });
+      .catch(this.setAxiosResponseError);
   };
   onTodoFormSubmit = (evt) => {
     evt.preventDefault();
@@ -35,9 +40,7 @@ export default class App extends React.Component {
       .then((res) => {
         this.setState({ ...this.state, todos: res.data.data });
       })
-      .catch((err) => {
-        this.setState({ ...this.state, error: err.response.data.message });
-      });
+      .catch(this.setAxiosResponseError);
   };
 
   componentDidMount() {
