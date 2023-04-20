41f608e91259278f968e5463559065a80c5b4723
Grayson Shogan <graysontoddshogan@gmail.com>
2023-04-20 09:58:59 -0700

    Got todos from server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
-
-const URL = 'http://localhost:9000/api/todos'
+import React from "react";
+import axios from "axios";
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  };
+  fetchAllTodos = () => {
+    axios
+      .get(URL)
+      .then((res) => {
+        this.setState({ ...this.state, todos: res.data.data });
+      })
+      .catch((err) => {});
+  };
+  componentDidMount() {
+    this.fetchAllTodos();
+  }
   render() {
-    return null
+    return (
+      <div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          {this.state.todos.map((td) => {
+            return <div key={td.id}>{td.name}</div>;
+          })}
+        </div>
+        <form id="todoForm">
+          <input type="text" placeholder="New Todo" />
+          <input type="submit" />
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    );
   }
 }
